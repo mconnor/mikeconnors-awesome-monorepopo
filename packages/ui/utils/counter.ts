758dd3cf3100bed8d9424d531dbd78f4c1040f9b
mike connor <mike@mikeconnor.tech>
2024-07-23 14:02:26 -0500

    linting, building

--- packages/ui/utils/counter.ts
+++ packages/ui/utils/counter.ts
@@ -1,10 +1,10 @@
 export function setupCounter(element: HTMLButtonElement) {
   let counter = 0;
-  const x;
+
   const setCounter = (count: number) => {
     counter = count;
     element.innerText = `count is ${counter}`;
   };
-  element.addEventListener("click", () => setCounter(++counter));
+  element.addEventListener('click', () => setCounter(++counter));
   setCounter(0);
 }
