fc18fd9b9f740b7a115987fa2ceb6a3e2c2914bb
mike connor <mike@mikeconnor.tech>
2024-07-23 12:09:34 -0500

    eslint flat config working in docs app

---
+++ apps/docs/eslint.config.mjs
@@ -0,0 +1,19 @@
+// import repoEslint from "@repo/eslint-config";
+
+import { FlatCompat } from "@eslint/eslintrc";
+import js from "@eslint/js";
+import path from "path";
+import { fileURLToPath } from "url";
+
+// mimic CommonJS variables -- not needed if using CommonJS
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const compat = new FlatCompat({
+  baseDirectory: __dirname, // optional; default: process.cwd()
+  resolvePluginsRelativeTo: __dirname, // optional
+  recommendedConfig: js.configs.recommended, // optional unless you're using "eslint:recommended"
+  allConfig: js.configs.all, // optional unless you're using "eslint:all"
+});
+
+export default [...compat.extends("@repo/eslint-config")];
