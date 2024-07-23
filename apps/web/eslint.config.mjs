1f8f28f114865890453af1c969fb6403028dc2de
mike connor <mike@mikeconnor.tech>
2024-07-23 15:12:11 -0500

    scripts

--- apps/web/eslint.config.mjs
+++ apps/web/eslint.config.mjs
@@ -1,18 +1,18 @@
-import repoEslint from "@repo/eslint-config";
+import repoEslint from '@repo/eslint-config';
 
 export default [
   ...repoEslint,
   {
     ignores: [
-      "**/_*.*",
-      "**/temp.js",
-      "*lock.yaml",
-      ".turbo/",
-      ".astro/",
-      "dist/",
-      "my-custom-cache-directory",
-      "src/env.d.ts",
-      ".vercel/",
+      '**/_*.*',
+      '**/temp.js',
+      '*lock.yaml',
+      '.turbo/',
+      '.astro/',
+      'dist/',
+      'my-custom-cache-directory',
+      'src/env.d.ts',
+      '.vercel/',
     ],
   },
 ];
--- packages/ui/eslint.config.mjs
+++ packages/ui/eslint.config.mjs
@@ -1,11 +1,11 @@
-import repoEslint from "@repo/eslint-config";
+import repoEslint from '@repo/eslint-config';
 
 export default [
   ...repoEslint,
   {
     languageOptions: {
-      ecmaVersion: "latest",
-      sourceType: "module",
+      ecmaVersion: 'latest',
+      sourceType: 'module',
 
       parserOptions: {
         project: true,
@@ -21,15 +21,15 @@ export default [
 
   {
     ignores: [
-      "**/_*.*",
-      "**/temp.js",
-      "*lock.yaml",
-      ".turbo/",
-      ".astro/",
-      "dist/",
-      "my-custom-cache-directory",
-      "src/env.d.ts",
-      ".vercel/",
+      '**/_*.*',
+      '**/temp.js',
+      '*lock.yaml',
+      '.turbo/',
+      '.astro/',
+      'dist/',
+      'my-custom-cache-directory',
+      'src/env.d.ts',
+      '.vercel/',
     ],
   },
 ];
