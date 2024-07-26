fdcd1ead71effcdf87aac02e969d2dcb91bac053
mike connor <mike@mikeconnor.tech>
2024-07-26 14:32:00 -0500

    linting working

---
+++ packages/astro-component/eslint.config.js
@@ -0,0 +1,37 @@
+import repoEslint from '@repo/eslint-config';
+
+export default [
+  ...repoEslint,
+  {
+    languageOptions: {
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+
+      parserOptions: {
+        project: 'tsconfig.json',
+      },
+    },
+  },
+  // {
+  //   rules: {
+  //     "@typescript-eslint/no-unused-vars": ["warn"],
+  //     // "lit/no-invalid-html": "warn",
+  //   },
+  // },
+
+  {
+    ignores: [
+      '**/_*.*',
+      '**/temp.js',
+      '*lock.yaml',
+      '.turbo/',
+      '.astro/',
+      'dist/',
+      'my-custom-cache-directory',
+      'src/env.d.ts',
+      '.vercel/',
+      '**/*.d.ts.map',
+      '**/*.d.ts',
+    ],
+  },
+];
---
+++ packages/astro-component/src/index.ts
@@ -0,0 +1,3 @@
+// import MyComponent from './MyComponent.astro'
+
+export { default as MyAstroComponent } from "./MyComponent.astro";
