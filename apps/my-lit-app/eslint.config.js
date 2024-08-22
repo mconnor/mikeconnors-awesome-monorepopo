54e59f174f5495e4a7335717d4c0e550978ac513
mike connor <mike@mikeconnor.tech>
2024-08-22 17:40:59 -0500

    delete lit app

--- apps/my-lit-app/eslint.config.js
+++
@@ -1,5 +0,0 @@
-import astroConfig from '@repo/eslint-config/astro';
-import tsLit from '@repo/eslint-config/lit-config';
-import eslintConfigPrettier from '@repo/eslint-config/prettier-config';
-
-export default [...astroConfig, ...tsLit, eslintConfigPrettier];
