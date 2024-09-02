2ebc64aae01e4fc358a7c9e2541703c96c54ab64
mike connor <mike@mikeconnor.tech>
2024-09-02 14:28:41 -0500

    ui lit setup

---
+++ packages/ui-components/ui-lit/eslint.config.js
@@ -0,0 +1,13 @@
+import eslintConfigPrettier from '@repo/eslint-config/prettier-config';
+import astroConfig from '@repo/eslint-config/astro';
+import typeCheckingDisable from '@repo/eslint-config/type-disable';
+import litConfig from '@repo/eslint-config/lit-config';
+
+export default [
+  ...astroConfig,
+  ...typeCheckingDisable,
+  ...litConfig,
+
+  eslintConfigPrettier,
+  { ignores: ['test/*.ts'] },
+];
