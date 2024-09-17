6d2d6e4476273a1788d4d8c5961c1a34c30e02db
mike connor <mike@mikeconnor.tech>
2024-09-17 14:23:53 -0500

    updates

---
+++ apps/pico-astro-theme/eslint.config.js
@@ -0,0 +1,14 @@
+import astroConfig from "@repo/eslint-config/astro";
+import reactConfig from "@repo/eslint-config/react";
+
+import eslintConfigPrettier from "@repo/eslint-config/prettier-config";
+
+export default [
+  ...astroConfig,
+  ...reactConfig,
+
+  eslintConfigPrettier,
+  {
+    ignores: [".prettierrc.mjs"],
+  },
+];
---
+++ apps/pico-astro-theme/src/components/Modal-react.jsx
@@ -0,0 +1,32 @@
+// Modal-react.jsx
+// description: The component was intended to generate Modal using React and React's createPortal() api.
+// obstacle: Astro or Vite has a problem to render nested component from UI integration like react.
+
+import { useState } from "react";
+import { createPortal } from "react-dom";
+
+// eslint-disable-next-line react/prop-types
+function ModalContent({ onClose }) {
+  return (
+    <div className="modal">
+      <div>I&apos;m a modal dialog</div>
+      <button onClick={onClose}>Close</button>
+    </div>
+  );
+}
+
+export default function PortalExample() {
+  const [showModal, setShowModal] = useState(false);
+  return (
+    <>
+      <button onClick={() => setShowModal(true)}>
+        Show modal using a portal
+      </button>
+      {showModal &&
+        createPortal(
+          <ModalContent onClose={() => setShowModal(false)} />,
+          document.body,
+        )}
+    </>
+  );
+}
