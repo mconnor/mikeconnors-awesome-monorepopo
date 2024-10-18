92d2e8b37339a3f0b945fdd51fc449005803cc81
mike connor <mike@mikeconnor.tech>
2024-10-17 19:44:16 -0500

    tsconfigs

--- apps/pico-astro-theme/eslint.config.js
+++ apps/pico-astro-theme/eslint.config.js
@@ -2,6 +2,5 @@
 import tsLint from '@repo/eslint-config/ts-lint-type-check';
 import astroConfig from '@repo/eslint-config/astro';
 import reactConfig from '@repo/eslint-config/react';
-import prettierConfig from '@repo/eslint-config/prettier';
 
-export default [...tsLint, ...astroConfig, ...reactConfig, prettierConfig];
+export default [...tsLint, ...astroConfig, ...reactConfig];
