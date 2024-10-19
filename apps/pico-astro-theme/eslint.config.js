cc77c79d979dfe62683fa0d1a048212ceba21902
mike connor <mike@mikeconnor.tech>
2024-10-19 18:50:58 -0500

    "build": "astro build",

--- apps/pico-astro-theme/eslint.config.js
+++
@@ -1,6 +0,0 @@
-// import tsLint from "@repo/eslint-config/ts-lint";
-import tsLint from '@repo/eslint-config/ts-lint-type-check';
-import astroConfig from '@repo/eslint-config/astro';
-import reactConfig from '@repo/eslint-config/react';
-
-export default [tsLint, astroConfig, reactConfig];
---
+++ apps/pico-astro-theme/eslint.config.mjs
@@ -0,0 +1,6 @@
+import tsLint from '@repo/eslint-config/ts-lint';
+// import tsLint from '@repo/eslint-config/ts-lint-type-check';
+import astroConfig from '@repo/eslint-config/astro';
+import reactConfig from '@repo/eslint-config/react';
+
+export default [...tsLint, ...astroConfig, ...reactConfig];
