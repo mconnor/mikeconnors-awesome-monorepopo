dfcf53fb8492cb8d24b81fabd70344e9059d0789
mike connor <mike@mikeconnor.tech>
2024-10-21 15:06:24 -0500

    @app/starlight

---
+++ apps/starlight/eslint.config.mjs
@@ -0,0 +1,8 @@
+// import tsLint from '@repo/eslint-config/ts-lint';
+import tsLint from '@repo/eslint-config/ts-lint-type-check';
+import astroConfig from '@repo/eslint-config/astro';
+
+// import jsonConfig from '@repo/eslint-config/json';
+// import regexConfig from '@repo/eslint-config/regex';
+
+export default [...tsLint, ...astroConfig];
