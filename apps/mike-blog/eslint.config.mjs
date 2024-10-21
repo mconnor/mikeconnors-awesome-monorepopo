31da03d0b0e80f07fdda632018404772c2266970
mike connor <mike@mikeconnor.tech>
2024-10-21 15:29:39 -0500

    "typescript": "^5.6.3",

--- apps/mike-blog/eslint.config.mjs
+++ apps/mike-blog/eslint.config.mjs
@@ -1,8 +1,8 @@
 // import tsLint from '@repo/eslint-config/ts-lint';
 import tsLint from '@repo/eslint-config/ts-lint-type-check';
 import astroConfig from '@repo/eslint-config/astro';
-
+import markdownConfig from '@repo/eslint-config/markdown';
 // import jsonConfig from '@repo/eslint-config/json';
 // import regexConfig from '@repo/eslint-config/regex';
 
-export default [...tsLint, ...astroConfig];
+export default [...tsLint, ...astroConfig,...markdownConfig];
