2eeeb9810e03a7cfce6f10e737f0c9d82f54222e
mike connor <mike@mikeconnor.tech>
2024-10-21 12:10:56 -0500

    delete solid ui

--- packages/ui-components/solid/eslint.config.js
+++
@@ -1,5 +0,0 @@
-// import tsLint from '@repo/eslint-config/ts-lint-type-check';
-import solidConfig from '@repo/eslint-config/solid';
-import tsLint from '@repo/eslint-config/ts-lint';
-
-export default [...tsLint, ...solidConfig];
