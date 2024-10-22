146980c74cc9b45047093dc9e5939d5d6e5e7d7f
mike connor <mike@mikeconnor.tech>
2024-10-22 17:13:20 -0500

    no longer ignore env.d.ts

---
+++ apps/mike-blog/src/env.d.ts
@@ -0,0 +1,12 @@
+/// <reference types="astro/client" />
+/// <reference path="../.astro/types.d.ts" />
+interface ImportMetaEnv {
+  readonly SENTRY_DSN: string;
+  readonly SENTRY_AUTH_TOKEN: string;
+  readonly ESLINT_USE_FLAT_CONFIG: string;
+  // more env variables...
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
