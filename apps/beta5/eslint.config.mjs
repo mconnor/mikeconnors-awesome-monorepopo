d7d35961454eae805c484bc0d61d25ee7a3443a3
mike connor <mike@mikeconnor.tech>
2024-10-22 18:20:55 -0500

    regexp test fix

---
+++ apps/beta5/eslint.config.mjs
@@ -0,0 +1,17 @@
+// import tsLint from '@repo/eslint-config/ts-lint';
+import tsLint from '@repo/eslint-config/ts-lint-type-check';
+import astroConfig from '@repo/eslint-config/astro';
+import markdownConfig from '@repo/eslint-config/markdown';
+// import jsonConfig from '@repo/eslint-config/json';
+import regexConfig from '@repo/eslint-config/regex';
+
+export default [...tsLint, ...astroConfig, ...markdownConfig,...regexConfig,
+    {
+        rules:
+        {
+            'no-useless-escape': 'warn',
+
+        }
+    }
+
+];
