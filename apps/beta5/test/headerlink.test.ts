976d6ba66e4203e7fc99b2a375921955dc568efd
mike connor <mike@mikeconnor.tech>
2024-10-22 19:33:31 -0500

    regex fix

---
+++ apps/beta5/test/headerlink.test.ts
@@ -0,0 +1,20 @@
+import { experimental_AstroContainer as AstroContainer } from 'astro/container';
+import { expect, test } from 'vitest';
+import HeaderLink from '../src/components/HeaderLink.astro';
+
+test('Card with slots', async () => {
+  const container = await AstroContainer.create();
+const result = await container.renderToString(HeaderLink, {
+    request: new Request("https://example.com/blog", {
+        headers: {
+          "x-some-secret-header": "test-value"
+        }
+      }),
+    slots: {
+        default: 'Card content',
+    },
+});
+
+  expect(result).toContain('This is a card');
+  expect(result).toContain('Card content');
+});
