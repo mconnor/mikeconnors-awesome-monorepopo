d05b3c83574ffcfe0713101c246c7f790cb35cd0
mike connor <mike@mikeconnor.tech>
2024-10-23 13:43:05 -0500

    Layout

--- apps/beta5/test/headerlink.test.ts
+++ apps/beta5/test/headerlink.test.ts
@@ -4,16 +4,16 @@ import HeaderLink from '../src/components/HeaderLink.astro';
 
 test('Card with slots', async () => {
   const container = await AstroContainer.create();
-const result = await container.renderToString(HeaderLink, {
-    request: new Request("https://example.com/blog", {
-        headers: {
-          "x-some-secret-header": "test-value"
-        }
-      }),
+  const result = await container.renderToString(HeaderLink, {
+    request: new Request('https://example.com/blog', {
+      headers: {
+        'x-some-secret-header': 'test-value',
+      },
+    }),
     slots: {
-        default: 'Card content',
+      default: 'Card content',
     },
-});
+  });
 
   expect(result).toContain('This is a card');
   expect(result).toContain('Card content');
