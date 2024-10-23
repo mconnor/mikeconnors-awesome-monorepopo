ec3148f83cca478320aa534a807d4982aa5c0f39
mike connor <mike@mikeconnor.tech>
2024-10-23 12:56:53 -0500

    add Layout

--- apps/beta5/eslint.config.mjs
+++ apps/beta5/eslint.config.mjs
@@ -5,13 +5,14 @@ import markdownConfig from '@repo/eslint-config/markdown';
 // import jsonConfig from '@repo/eslint-config/json';
 import regexConfig from '@repo/eslint-config/regex';
 
-export default [...tsLint, ...astroConfig, ...markdownConfig,...regexConfig,
-    {
-        rules:
-        {
-            'no-useless-escape': 'warn',
-
-        }
-    }
-
+export default [
+  ...tsLint,
+  ...astroConfig,
+  ...markdownConfig,
+  ...regexConfig,
+  {
+    rules: {
+      'no-useless-escape': 'warn',
+    },
+  },
 ];
