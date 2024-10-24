1f9af56d271fc64eb91bcee98d1f0e81b5f963c4
mike connor <mike@mikeconnor.tech>
2024-10-24 17:40:16 -0500

    types

---
+++ apps/starlight/types/postcss.d.ts
@@ -0,0 +1,14 @@
+declare module 'postcss-jit-props' {
+  const postcssJitProps: any;
+  export default postcssJitProps;
+}
+
+declare module 'postcss-pow' {
+  const config: any;
+  export default config;
+}
+
+declare module 'postcss-import' {
+  const config: any;
+  export default config;
+}
