f73887e854142768233aaacdc3b0c99c1c28d988
mike connor <mike@mikeconnor.tech>
2024-10-24 19:50:49 -0500

    fussing with config

--- packages/config-eslint/reactConfig.mjs
+++ packages/config-eslint/reactConfig.mjs
@@ -1,12 +1,43 @@
 // @ts-check
-import reactPlugin from 'eslint-plugin-react';
-import tseslint from 'typescript-eslint';
-import disableTypeChecking from './typeCheckingDisable.mjs';
+import react from 'eslint-plugin-react';
+// import tseslint from 'typescript-eslint';
+// import disableTypeChecking from './typeCheckingDisable.mjs';
 
-export default tseslint.config(
+// export default tseslint.config(
+//   {
+//     ...reactPlugin.configs.recommended,
+//     ...reactPlugin.configs['recommended-with-jsx'],
+//     languageOptions: {
+//       parserOptions: {
+//         ecmaFeatures: {
+//           jsx: true,
+//         },
+//       },
+//     },
+//     rules: {
+//       'react/react-in-jsx-scope': 'off',
+//       'react/no-unescaped-entities': 'off',
+//     },
+//   },
+//   {
+//     files: ['**/*.jsx'],
+//     ...disableTypeChecking,
+//   },
+//   {
+//     settings: {
+//       react: {
+//         version: 'detect',
+//       },
+//     },
+//   },
+// );
+
+export default [
   {
-    ...reactPlugin.configs.recommended,
-    ...reactPlugin.configs['recommended-with-jsx'],
+    files: ['**/*.{js,jsx,mjs,cjs,ts,tsx}'],
+    plugins: {
+      react,
+    },
     languageOptions: {
       parserOptions: {
         ecmaFeatures: {
@@ -15,19 +46,10 @@ export default tseslint.config(
       },
     },
     rules: {
-      'react/react-in-jsx-scope': 'off',
-      'react/no-unescaped-entities': 'off',
+      // ... any rules you want
+      'react/jsx-uses-react': 'error',
+      'react/jsx-uses-vars': 'error',
     },
+    // ... others are omitted for brevity
   },
-  {
-    files: ['**/*.jsx'],
-    ...disableTypeChecking,
-  },
-  {
-    settings: {
-      react: {
-        version: 'detect',
-      },
-    },
-  },
-);
+];
