c0c623bf26ea08a16e89a7b6455c46936e9642dc
mike connor <mike@mikeconnor.tech>
2024-10-30 14:42:25 -0500

    eslint for svelte app

---
+++ apps/svelte-app/eslint.config.mjs
@@ -0,0 +1,7 @@
+import tsLint from "@repo/eslint-config/ts-lint";
+
+import svelteConfig from "@repo/eslint-config/svelte";
+// import reactConfig from '@repo/eslint-config/react';
+// import svelte from '@repo/eslint-config/svelte';
+
+export default [...tsLint, ...svelteConfig];
