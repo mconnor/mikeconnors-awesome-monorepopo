653f23e52ebf5389f0c3887d49752b5e6e516a1f
mike connor <mike@mikeconnor.tech>
2024-10-30 14:50:16 -0500

    format

--- apps/svelte-app/eslint.config.mjs
+++ apps/svelte-app/eslint.config.mjs
@@ -1,6 +1,6 @@
-import tsLint from "@repo/eslint-config/ts-lint";
+import tsLint from '@repo/eslint-config/ts-lint';
 
-import svelteConfig from "@repo/eslint-config/svelte";
+import svelteConfig from '@repo/eslint-config/svelte';
 // import reactConfig from '@repo/eslint-config/react';
 // import svelte from '@repo/eslint-config/svelte';
 
