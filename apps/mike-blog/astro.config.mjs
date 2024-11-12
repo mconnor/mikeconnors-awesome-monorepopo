8425d1d5fa270ca2b46a1351e7ca4e6c2d49238b
mike connor <mike@mikeconnor.tech>
2024-11-12 17:35:40 -0600

    remove test, node 20

--- apps/mike-blog/astro.config.mjs
+++ apps/mike-blog/astro.config.mjs
@@ -1,26 +1,24 @@
-// import sentry from '@sentry/astro';
-
 import mdx from '@astrojs/mdx';
-import vercel from '@astrojs/vercel/static';
+import vercel from '@astrojs/vercel';
 import { defineConfig } from 'astro/config';
 import icon from 'astro-icon';
-// import { loadEnv } from 'vite';
 
-// const { SENTRY_AUTH_TOKEN } = loadEnv(
-//   process.env.SENTRY_AUTH_TOKEN,
-//   process.cwd(),
-//   '',
-// );
+
 
 export default defineConfig({
   outDir: 'dist',
   compressHTML: true,
   // trailingSlash: 'never',
   cacheDir: './cache-directory',
-  redirects: {
-    '/index': '/about',
-  },
 
+
+  // env: {
+  //   schema: {
+  //     SENTRY_DSN: envField.string({ context: "server", access: "private"}),
+  //     SENTRY_AUTH_TOKEN: envField.string({ context: "server", access: "private"}),
+  //   }
+  // }
+  
   site: 'https://staging.mikeconnor.tech/',
 
   image: {
@@ -38,23 +36,6 @@ export default defineConfig({
     mdx({
       drafts: true,
     }),
-    // sentry({
-    //   dsn: 'https://b620d87b24b1fda6b0064305d7ca46e2@o4506328206802944.ingest.us.sentry.io/4506770210291712',
-    //   release: '1.0.0',
-    //   environment: import.meta.env.MODE,
-    //   sourceMapsUploadOptions: {
-    //     org: 'mike-connor',
-    //     project: 'astro-openprops-blog-w-tags',
-    //     authToken: import.meta.env.SENTRY_AUTH_TOKEN,
-    //   },
-    // }),
-
-    // icon({
-    //   include: {
-    //     mdi: ['*'], // (Default) Loads entire Material Design Icon set
-    //     // mdi: ['account'], // Loads only Material Design Icon's "account" SVG
-    //   },
-    // }),
   ],
  output: 'server',
   adapter: vercel(),
--- apps/mike-blog/src/app.config.mts
+++ apps/mike-blog/src/app.config.mts
@@ -6,3 +6,9 @@ export const SITE_DESCRIPTION =
   'Astrofy is a free and open-source template for your Personal Portfolio Website built with Astro and TailwindCSS. Create in minutes a website with Blog, CV, Project Section, Store and RSS Feed.';
 // export const GENERATE_SLUG_FROM_TITLE = true;
 // export const TRANSITION_API = true;
+
+export enum Links {
+  LINKEDIN = 'https://www.linkedin.com/in/michaelgconnor/',
+  GITHUB = 'https://www.github.com/mconnor',
+  TWITTER = 'https://www.twitter.com/mikeconnor504'
+}
