63a489736530bc0b65d9b0f86cd4eb0dc41270c6
mike connor <mike@mikeconnor.tech>
2024-11-26 18:20:45 -0600

    simple-blog

--- apps/simple-blog-beta5/eslint.config.mjs
+++ apps/simple-blog-beta5/eslint.config.mjs
@@ -3,7 +3,7 @@ import astLint from '@repo/eslint-config/astro.lint.config';
 // import markdownConfig from '@repo/eslint-config/md.lint.config';
 // import jsonConfig from '@repo/eslint-config/json';
 // import regexConfig from '@repo/eslint-config/regex';
-import tsLint from '@repo/eslint-config/ts.lint.config';
+import tsLint from '@repo/eslint-config/ts-lint';
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [...tsLint, ...astLint];
