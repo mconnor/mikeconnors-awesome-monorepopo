17522428908e21dc339d5f6247e674dcb33a826f
mike connor <mike@mikeconnor.tech>
2025-02-01 16:59:22 -0600

    lightening css

--- apps/astrodemo/astro.config.mjs
+++ apps/astrodemo/astro.config.mjs
@@ -2,7 +2,8 @@ import mdx from '@astrojs/mdx';
 import vercel from '@astrojs/vercel';
 
 import { defineConfig } from 'astro/config';
-
+import browserslist from 'browserslist';
+import { browserslistToTargets } from 'lightningcss';
 // import { loadEnv } from 'vite';
 //const { SECRET_PASSWORD } = loadEnv(process.env.NODE_ENV, process.cwd(), '');
 // import { SENTRY_DSN, SENTRY_AUTH_TOKEN } from 'astro:env/server';
@@ -13,6 +14,17 @@ export default defineConfig({
   output: 'server',
   adapter: vercel(),
   site: 'https://example.com',
+  vite: {
+    css: {
+      transformer: 'lightningcss',
+      lightningcss: {
+        targets: browserslistToTargets(browserslist('>= 0.25%')),
+        drafts: {
+          customMedia: true,
+        },
+      },
+    },
+  },
 
   // env: {
   //   schema: {
