d4ba581da10ef6df0c2e80caf37e1a3cd156d82d
mike connor <mike@mikeconnor.tech>
2025-02-04 18:51:10 -0600

    ui

--- packages/ui-components/react/src/ReactCounter.tsx
+++ packages/ui-components/react/src/ReactCounter.tsx
@@ -1,6 +1,6 @@
 import { type JSX, useState } from 'react';
 
-import { MyButton } from './components/Button';
+import { MyButton } from '#components/Button.jsx';
 
 interface Props {
   children: React.ReactNode;
