f3593abd8c5a1cacb9ae50193290913f0ac7df48
mike connor <mike@mikeconnor.tech>
2025-02-26 13:42:42 -0600

    script

---
+++ packages/scripts/capitalizeString.ts
@@ -0,0 +1,13 @@
+/**
+ * Capitalizes the first letter of the given string.
+ * @param str - The string to capitalize.
+ * @returns The capitalized string.
+ */
+function capitalizeString(str: string): string {
+    if (str.length === 0) {
+        return str;
+    }
+    return str.charAt(0).toUpperCase() + str.slice(1);
+}
+
+export default capitalizeString;
