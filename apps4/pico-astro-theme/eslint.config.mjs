5647f766d06afdcfc484390b336e38c7788165ad
mike connor <mike@mikeconnor.tech>
2025-03-16 13:34:06 -0500

    delete app

--- apps4/pico-astro-theme/eslint.config.mjs
+++
@@ -1,8 +0,0 @@
-
-import reactConfig from '@repo/eslint-config/react.lint.config';
-import tsLint from '@repo/eslint-config/ts-lint';
-
-/** @type {import('eslint').Linter.Config[]} */
-const config =  [...tsLint, ...reactConfig];
-
-export default config;
