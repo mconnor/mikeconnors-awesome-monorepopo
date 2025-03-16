4bc98e7b6dc6f159a90fffd875d3cf032eea278f
mike connor <mike@mikeconnor.tech>
2025-03-16 13:33:05 -0500

    blog tailwind

---
+++ apps/blog-tailwind/eslint.config.mjs
@@ -0,0 +1,11 @@
+import markdownConfig from '@repo/eslint-config/md-config';
+import tsLint from '@repo/eslint-config/ts-lint';
+// import turboConfig from '@repo/eslint-config/turbo-config';
+
+// import jsonConfig from '@repo/eslint-config/json';
+// import regexConfig from '@repo/eslint-config/regex';
+
+/** @type {import("eslint").Linter.Config} */
+const config = [...tsLint, ...markdownConfig];
+
+export default config;
