a3eed5a8efbba96d5646c5dc7562a0c8fdf1145e
mike connor <mike@mikeconnor.tech>
2025-03-29 12:37:36 -0500

    tailwindcss in astrodemo

--- apps/astrodemo/astro.config.mjs
+++ apps/astrodemo/astro.config.mjs
@@ -1,16 +1,16 @@
 import mdx from '@astrojs/mdx';
 import vercel from '@astrojs/vercel';
-
+import tailwindcss from '@tailwindcss/vite';
 import { defineConfig } from 'astro/config';
-import browserslist from 'browserslist';
-import { browserslistToTargets } from 'lightningcss';
 
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
   adapter: vercel(),
   site: 'https://example.com',
-  vite: {},
+  vite: {
+    plugins: [tailwindcss()],
+  },
 
   integrations: [mdx()],
 });
