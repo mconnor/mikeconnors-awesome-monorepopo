a5e5964259e092db5df94bc4228293c5511d0615
mike connor <mike@mikeconnor.tech>
2025-04-18 15:03:33 -0500

    delet

--- apps-to-off-ramp/apps-svelte/svelte-demo/eslint.config.mjs
+++
@@ -1,8 +0,0 @@
-import svelte from '@repo/eslint-config/svelte5-app';
-
-import tsLint from '@repo/eslint-config/ts-lint';
-
-/** @type {import("eslint").Linter.Config} */
-const config = [...tsLint, ...svelte];
-
-export default config;
