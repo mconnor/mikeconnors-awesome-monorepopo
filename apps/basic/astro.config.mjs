d60a1624f0495bd4ee90fe4f2d57e0acbd9baff2
mike connor <mike@mikeconnor.tech>
2025-04-28 20:19:23 -0500

    basic app for daisy testing

--- apps/basic/astro.config.mjs
+++ apps/basic/astro.config.mjs
@@ -1,5 +1,18 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
+import tailwindcss from '@tailwindcss/vite';
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  vite: {
+    ssr: {
+      noExternal: ['@repo/design-system'],
+    },
+    plugins: [
+      tailwindcss(),
+      // visualizer({
+      //   emitFile: true,
+      //   filename: 'stats.html',
+      // }),
+    ],
+  },
+});
