bd916da1263bd241ebb236b0a5f493fda7d26565
mike connor <mike@mikeconnor.tech>
2025-05-28 17:12:19 -0500

    prune apps

--- apps/hackernews/eslint.config.mjs
+++
@@ -1,7 +0,0 @@
-import tsLint from '@repo/eslint-config/ts-lint';
-// import jsonLint from '@repo/eslint-config/jsonLint';
-
-/** @type {import("eslint").Linter.Config} */
-// const config = [...tsLint, ...jsonLint];
-
-export default tsLint;
