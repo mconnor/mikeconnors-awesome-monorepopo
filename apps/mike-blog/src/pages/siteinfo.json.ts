e4d6b033334707290928907904ae21110ab8df1a
mike connor <mike@mikeconnor.tech>
2025-06-11 15:08:21 -0500

    prose

--- apps/mike-blog/src/pages/siteinfo.json.ts
+++
@@ -1,6 +0,0 @@
-import type { APIContext } from 'astro';
-
-export function GET({ generator, site }: APIContext) {
-  const body = JSON.stringify({ generator, site });
-  return new Response(body);
-}
