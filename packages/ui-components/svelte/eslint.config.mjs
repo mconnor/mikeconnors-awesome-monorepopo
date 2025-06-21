a421c117bc17009f22c983610a40ab50b0303a92
mike connor <mike@mikeconnor.tech>
2025-06-21 18:07:37 -0500

    svelte

--- packages/ui-components/svelte/eslint.config.mjs
+++ packages/ui-components/svelte/eslint.config.mjs
@@ -1,4 +1,4 @@
-import svelte from "@repo/eslint-config/svelte5-app";
+import svelte from '@repo/eslint-config/svelte5-app';
 // import eslintConfigPrettier from '@repo/eslint-config/prettier';
 
 export default svelte;
