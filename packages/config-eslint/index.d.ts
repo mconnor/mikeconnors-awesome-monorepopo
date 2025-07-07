f6f6757bc5c15274393d78a366f95f790cb9385f
mike connor <mike@mikeconnor.tech>
2025-07-07 13:52:10 -0500

    lint d.ts

---
+++ packages/config-eslint/index.d.ts
@@ -0,0 +1,17 @@
+import type { Linter } from 'eslint';
+
+// Export types for all your configurations
+export declare const tsLint: Linter.Config[];
+export declare const nextConfig: Linter.Config[];
+export declare const reactInternal: Linter.Config[];
+export declare const vueConfig: Linter.Config[];
+export declare const svelteConfig: Linter.Config[];
+export declare const litConfig: Linter.Config[];
+export declare const markdownConfig: Linter.Config[];
+export declare const tailwindLint: Linter.Config[];
+export declare const turboConfig: Linter.Config[];
+export declare const ignores: Linter.Config[];
+export declare const regexConfig: Linter.Config[];
+export declare const solidConfig: Linter.Config[];
+export declare const simpleImportsSort: Linter.Config[];
+export declare const typeDisable: Linter.Config[];
