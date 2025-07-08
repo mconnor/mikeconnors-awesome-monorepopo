bd4415b6aeca59e5dd38cfff014438e69231a5cd
mike connor <mike@mikeconnor.tech>
2025-07-08 17:39:06 -0500

    env.d.ts

--- apps/mike-blog/src/env.d.ts
+++ apps/mike-blog/src/env.d.ts
@@ -1,6 +1,8 @@
 interface ImportMetaEnv {
   readonly SENTRY_DSN: string;
   readonly SENTRY_AUTH_TOKEN: string;
+  readonly DOMAIN: string;
+  readonly USER_NAME: string;
   // more env variables...
 }
 
