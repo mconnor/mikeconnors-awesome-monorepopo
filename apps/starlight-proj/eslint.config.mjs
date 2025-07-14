1d53d74e333541d4ef465f7b9bc918d1a381b3d2
mike connor <mike@mikeconnor.tech>
2025-07-13 19:16:54 -0500

    starlight app

---
+++ apps/starlight-proj/eslint.config.mjs
@@ -0,0 +1,7 @@
+import markdownConfig from '@repo/eslint-config/md-config';
+
+
+/** @type {import("eslint").Linter.Config} */
+// const config = [...tsLint, ...markdownConfig];
+
+export default markdownConfig;
