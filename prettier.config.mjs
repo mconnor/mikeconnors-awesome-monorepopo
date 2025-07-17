340dcd138fbb1b3bfbacaf6c2287327ee9d63c56
mike connor <mike@mikeconnor.tech>
2025-07-17 12:39:44 -0500

    fix multi app… build react

--- prettier.config.mjs
+++ prettier.config.mjs
@@ -1,7 +1,36 @@
-// .prettierrc.mjs
-/** @type {import("prettier").Config} */
-export default {
-
+/**
+ * @see https://prettier.io/docs/configuration
+ * @type {import("prettier").Config}
+ */
+const config = {
+  plugins: [
+    '@prettier/plugin-oxc',
+    'prettier-plugin-toml',
+    'prettier-plugin-astro',
+    'prettier-plugin-tailwindcss',
+  ],
+  overrides: [
+    {
+      files: '**/*.{js,mjs,cjs,jsx}',
+      options: {
+        parser: 'oxc',
+      },
+    },
+    {
+      files: '**/*.{ts,mts,cts,tsx}',
+      options: {
+        parser: 'oxc-ts',
+      },
+    },
+    {
+      files: '**/*.astro',
+      options: {
+        parser: 'astro',
+      },
+    },
+  ],
+  tailwindStylesheet: './src/styles/global.css',
   singleQuote: true,
   experimentalTernaries: true,
 };
+export default config;
