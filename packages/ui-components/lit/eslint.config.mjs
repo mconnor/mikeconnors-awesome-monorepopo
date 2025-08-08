c854694c7d8ac02b5dc618408e8e08f87cb92914
mike connor <mike@mikeconnor.tech>
2025-08-08 13:19:05 -0500

    delete lit

--- packages/ui-components/lit/eslint.config.mjs
+++
@@ -1,7 +0,0 @@
-import tsLint from '@repo/eslint-config/ts-lint';
-import litConfig from '@repo/eslint-config/lit-config';
-
-/** @type {import("eslint").Linter.Config} */
-const config = [...tsLint, ...litConfig];
-
-export default config;
