e24945bb553ac755f276b3af207bcbaa339f078d
mike connor <mike@mikeconnor.tech>
2025-09-11 17:29:53 -0500

    workspaces

---
+++ apps/mike-blog/vite.config.ts
@@ -0,0 +1,15 @@
+import { uiConfig } from '@repo/vitest-config/ui';
+
+export default uiConfig;
+
+// import { getViteConfig } from 'astro/config';
+
+// export default getViteConfig({
+//   test: {
+//     // Vitest configuration options
+//     // assetsInclude: ['**/*.jpg'],
+//     coverage: {
+//       provider: 'istanbul' // or 'v8'
+//     },
+//   },
+// });
