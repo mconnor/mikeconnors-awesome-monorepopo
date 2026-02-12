441d7984f36ac5791067fdf1469faacb74361407
mike connor <mike@mikeconnor.tech>
2026-02-12 15:40:41 -0600

    lint

--- packages/ui-components/react/eslint.config.mjs
+++ packages/ui-components/react/eslint.config.mjs
@@ -1,4 +1,7 @@
 import reactConfig from '@repo/eslint-config/react-internal';
 import tsLint from '@repo/eslint-config/ts-lint';
 
-export default [ ...reactConfig];
+/** @type {import('eslint').Linter.Config[]} */
+const config = [...tsLint, ...reactConfig];
+
+export default config;
